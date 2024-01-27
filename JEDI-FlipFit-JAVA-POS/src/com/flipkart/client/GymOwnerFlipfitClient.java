ef7ba20b881843383f075721aa9cdf80921521e4
shreyasFlipkart <shreyas.ketkar@flipkart.com>
2024-01-27 23:32:33 +0530

    exception handling for gymOwnerClientMainPage

--- JEDI-FlipFit-JAVA-POS/src/com/flipkart/client/GymOwnerFlipfitClient.java
+++ JEDI-FlipFit-JAVA-POS/src/com/flipkart/client/GymOwnerFlipfitClient.java
@@ -92,8 +92,10 @@ public class GymOwnerFlipfitClient {
         System.out.println(YELLOW_COLOR+"WELCOME "+gymOwnerId+" !!\nWhat you what to do\nLogin TIME: "+currentTime+RESET_COLOR);
 
         boolean isContinue = true;
+        int choice  = 1;
 
         while(isContinue){
+            System.out.println("Enter your choice (1, 2, 3, 4, 5, 6, 7, 8, 9 ): \n");
             System.out.println("" +
                     "0: View Profile\n" +
                     "1: Edit Profile\n" +
@@ -115,185 +117,221 @@ public class GymOwnerFlipfitClient {
             if(g.getIsApproved()==2){
                 System.out.println("Your approval request is under process.");
             }
-
-            int choice = scanner.nextInt();
-
-
-            switch (choice) {
-                /* Take input from user for all service parameters ( Make the menu ) */
-                case 0:
-                    GymOwner owner = gymOwnerService.viewGymOwnerProfile(gymOwnerId);
-                    if (owner.getUserID().equals("none")) System.out.println("User not found");
-                    System.out.println("Profile: \nUsername: " + owner.getUserName() + "\nPAN Card: " + owner.getPanNumber() + "\nEmail: " + owner.getEmail());
-                    break;
-                case 1:
-                    //edit profile
-                    editGymOwnerProfile(gymOwnerId);
-                    return ;
-                case 2:
-                    List<GymCentre> allGymCentres = gymCentreService.getAllCentresByOwmerId(gymOwnerId);
-                    util.printGymCentres(allGymCentres);
-                    break;
-                case 3:
-                    gymOwnerService.requestGymOwnerApproval(gymOwnerId);
-                    break;
-
-                case 4:
-
-                    if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==0){
-                        System.out.println(RED_COLOR + "Please get yourself approved first!!!" + RESET_COLOR);
+            try{
+                choice = scanner.nextInt();
+                switch (choice) {
+                    /* Take input from user for all service parameters ( Make the menu ) */
+                    case 0:
+                        GymOwner owner = gymOwnerService.viewGymOwnerProfile(gymOwnerId);
+                        if (owner.getUserID().equals("none")) System.out.println("User not found");
+                        System.out.println("Profile: \nUsername: " + owner.getUserName() + "\nPAN Card: " + owner.getPanNumber() + "\nEmail: " + owner.getEmail());
                         break;
-                    }
-
-                    if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==2){
-                        System.out.println(YELLOW_COLOR + "Please wait for admin to approve you!" + RESET_COLOR);
+                    case 1:
+                        //edit profile
+                        editGymOwnerProfile(gymOwnerId);
+                        return ;
+                    case 2:
+                        List<GymCentre> allGymCentres = gymCentreService.getAllCentresByOwmerId(gymOwnerId);
+                        util.printGymCentres(allGymCentres);
                         break;
-                    }
-
-                    System.out.println("Enter gym centre id: ");
-                    String gymId = scanner.next();
-
-                    System.out.println("Enter Gym Centre name: ");
-                    String gymCentreName = scanner.next();
-
-                    System.out.println("Enter Gym Centre GSTIN: ");
-                    String gstin = scanner.next();
-                    if(!validate.isGstValid(gstin)){
-                        System.out.println("Please enter valid GST Number");
+                    case 3:
+                        gymOwnerService.requestGymOwnerApproval(gymOwnerId);
                         break;
-                    }
 
-                    System.out.println("Enter Gym Centre Location:\n1. North Bangalore\n2. South Bangalore\n3. West Bangalore \n4. East Bangalore \n");
-                    int gymLocationChoice = scanner.nextInt();
-                    String city = "Bangalore";
-                    switch (gymLocationChoice) {
-                        case 1:
-                            city = "North Bangalore";
+                    case 4:
+
+                        if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==0){
+                            System.out.println(RED_COLOR + "Please get yourself approved first!!!" + RESET_COLOR);
                             break;
-                        case 2:
-                            city = "South Bangalore";
+                        }
+
+                        if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==2){
+                            System.out.println(YELLOW_COLOR + "Please wait for admin to approve you!" + RESET_COLOR);
                             break;
-                        case 3:
-                            city = "West Bangalore";
-                            break;
-                        case 4:
-                            city = "East Bangalore";
-                            break;
-                    }
+                        }
 
-                    System.out.println("Enter Gym Centre capacity: ");
-                    int capacity = scanner.nextInt();
+                        System.out.println("Enter gym centre id: ");
+                        String gymId = scanner.next();
 
-                    System.out.println("Enter price: ");
-                    int price = scanner.nextInt();
+                        System.out.println("Enter Gym Centre name: ");
+                        String gymCentreName = scanner.next();
 
-                    gymCentreService.addCenter(
-                            new GymCentre(
-                                    gymId,
-                                    gymOwnerId,
-                                    gymCentreName,
-                                    gstin,
-                                    city,
-                                    capacity,
-                                    price
-                            )
-                    );
-                    break;
 
-                case 5:
+                        boolean isValidGst = false;
+                        String gstin = "";
+                        while(!isValidGst){
+                            System.out.println("Enter Gym Centre GSTIN: ");
+                            gstin = scanner.next();
+                            if(!validate.isGstValid(gstin)){
+                                System.out.println("Please enter valid GST Number");
+                            }
+                            else isValidGst = true;
+                        }
 
-                    System.out.println("Enter Gym Centre Id: ");
-                    String gymCentreId = scanner.next();
 
-                    gymCentreService.requestGymCentreApproval(gymCentreId);
-                    break;
+                        String city = "Bangalore";
+                        boolean isValidGymCentre = false;
+                        while(!isValidGymCentre){
 
-                case 6:
+                            System.out.println("Enter Gym Centre Location:\n1. North Bangalore\n2. South Bangalore\n3. West Bangalore \n4. East Bangalore \n");
+                            System.out.println("Enter your choice (1, 2, 3, 4 ): \n");
+                            isValidGymCentre = true;
+                            int gymLocationChoice = 1;
+                            try
+                            {
+                                gymLocationChoice = scanner.nextInt();
+                                switch (gymLocationChoice) {
+                                    case 1:
+                                        city = "North Bangalore";
+                                        break;
+                                    case 2:
+                                        city = "South Bangalore";
+                                        break;
+                                    case 3:
+                                        city = "West Bangalore";
+                                        break;
+                                    case 4:
+                                        city = "East Bangalore";
+                                        break;
+                                    default:
+                                        isValidGymCentre = false;
+                                        System.out.println(INVALID_CHOICE_ERROR);
+                                        break;
+                                }
+                            }
+                            catch (Exception e) {
+                                isValidGymCentre = false;
+                                System.out.println("Invalid input, please enter a valid numerical value.");
+                                scanner.nextLine(); // Clear the buffer
+                            }
 
-                    if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==0){
-                        System.out.println(RED_COLOR + "Please get yourself approved first!!!" + RESET_COLOR);
+
+                        }
+
+
+                        System.out.println("Enter Gym Centre capacity: ");
+                        int capacity = scanner.nextInt();
+
+                        System.out.println("Enter price: ");
+                        int price = scanner.nextInt();
+
+                        gymCentreService.addCenter(
+                                new GymCentre(
+                                        gymId,
+                                        gymOwnerId,
+                                        gymCentreName,
+                                        gstin,
+                                        city,
+                                        capacity,
+                                        price
+                                )
+                        );
                         break;
-                    }
 
-                    if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==2){
-                        System.out.println(YELLOW_COLOR + "Please wait for admin to approve you!" + RESET_COLOR);
+                    case 5:
+
+                        System.out.println("Enter Gym Centre Id: ");
+                        String gymCentreId = scanner.next();
+
+                        gymCentreService.requestGymCentreApproval(gymCentreId);
                         break;
-                    }
 
-                    boolean isAdding = true;
-                    String centreId = null;
+                    case 6:
 
-                    List<Slot> newSlotList = new ArrayList<>();
-                    while (isAdding) {
-                        System.out.println("Enter new slot ID: ");
-                        String slotId = scanner.next();
+                        if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==0){
+                            System.out.println(RED_COLOR + "Please get yourself approved first!!!" + RESET_COLOR);
+                            break;
+                        }
 
+                        if(gymOwnerService.viewGymOwnerProfile(gymOwnerId).getIsApproved()==2){
+                            System.out.println(YELLOW_COLOR + "Please wait for admin to approve you!" + RESET_COLOR);
+                            break;
+                        }
+
+                        boolean isAdding = true;
+                        String centreId = null;
+
+                        List<Slot> newSlotList = new ArrayList<>();
+                        while (isAdding) {
+                            System.out.println("Enter new slot ID: ");
+                            String slotId = scanner.next();
+
+                            System.out.println("Enter Gym Centre ID: ");
+                            centreId = scanner.next();
+
+                            if(gymCentreService.getGymCentreById(centreId).getIsApproved()==0){
+                                System.out.println(RED_COLOR + "Please get your center approved first!!!" + RESET_COLOR);
+                                isAdding=false;
+                                break;
+                            }
+
+                            if(gymCentreService.getGymCentreById(centreId).getIsApproved()==2){
+                                System.out.println(YELLOW_COLOR + "Please wait for admin to approve your gym!" + RESET_COLOR);
+                                isAdding=false;
+                                break;
+                            }
+
+                            System.out.println("Enter time in 24h format (hh:mm:ss) : ");
+                            String time = scanner.next();
+
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                            LocalTime localTime = LocalTime.parse(time, formatter);
+
+                            newSlotList.add(new Slot(
+                                    slotId,
+                                    centreId,
+                                    localTime
+                            ));
+
+                            System.out.println("Do you want to enter more slots (yes/no)?: ");
+                            String addChoice = scanner.next();
+                            addChoice = addChoice.toLowerCase();
+
+                            if (addChoice.equals("n") || addChoice.equals("no")) {
+                                isAdding = false;
+                            }
+                        }
+
+                        if(centreId!=null)slotService.addSlotsForGym(centreId, newSlotList);
+                        break;
+                    case 7:
                         System.out.println("Enter Gym Centre ID: ");
-                        centreId = scanner.next();
-
-                        if(gymCentreService.getGymCentreById(centreId).getIsApproved()==0){
-                            System.out.println(RED_COLOR + "Please get your center approved first!!!" + RESET_COLOR);
-                            isAdding=false;
-                            break;
+                        String id = scanner.next();
+                        List<Slot> slotList = slotService.getAllSlotsByCentre(id);
+                        for (Slot slot : slotList) {
+                            System.out.println("Slot ID: " + slot.getSlotId() + ", Centre ID: " + slot.getCentreID() + ", Time: " + slot.getTime());
                         }
+                        break;
 
-                        if(gymCentreService.getGymCentreById(centreId).getIsApproved()==2){
-                            System.out.println(YELLOW_COLOR + "Please wait for admin to approve your gym!" + RESET_COLOR);
-                            isAdding=false;
-                            break;
-                        }
+                    case 8:
+                        System.out.println("Enter Slot ID: ");
+                        String slotIdToDelete = scanner.next();
+                        boolean flag = slotService.deleteSlotById(slotIdToDelete);
+                        if (flag) System.out.println("Successfully Deleted");
+                        else System.out.println("Slot not found");
 
-                        System.out.println("Enter time in 24h format (hh:mm:ss) : ");
-                        String time = scanner.next();
+                        break;
 
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-                        LocalTime localTime = LocalTime.parse(time, formatter);
+                    case 9:
+                        System.out.println("Do you wish to continue? (yes/no)");
+                        String ans = scanner.next();
+                        if(ans.equals("n") || ans.equals("no")) isContinue = false;
+                        break;
 
-                        newSlotList.add(new Slot(
-                                slotId,
-                                centreId,
-                                localTime
-                        ));
-
-                        System.out.println("Do you want to enter more slots (yes/no)?: ");
-                        String addChoice = scanner.next();
-                        addChoice = addChoice.toLowerCase();
-
-                        if (addChoice.equals("n") || addChoice.equals("no")) {
-                            isAdding = false;
-                        }
-                    }
-
-                    if(centreId!=null)slotService.addSlotsForGym(centreId, newSlotList);
-                    break;
-                case 7:
-                    System.out.println("Enter Gym Centre ID: ");
-                    String id = scanner.next();
-                    List<Slot> slotList = slotService.getAllSlotsByCentre(id);
-                    for (Slot slot : slotList) {
-                        System.out.println("Slot ID: " + slot.getSlotId() + ", Centre ID: " + slot.getCentreID() + ", Time: " + slot.getTime());
-                    }
-                    break;
-
-                case 8:
-                    System.out.println("Enter Slot ID: ");
-                    String slotIdToDelete = scanner.next();
-                    boolean flag = slotService.deleteSlotById(slotIdToDelete);
-                    if (flag) System.out.println("Successfully Deleted");
-                    else System.out.println("Slot not found");
-
-                    break;
-
-                case 9:
-                    System.out.println("Do you wish to continue? (yes/no)");
-                    String ans = scanner.next();
-                    if(ans.equals("n") || ans.equals("no")) isContinue = false;
-                    break;
-
-                default:
-                    System.out.println(INVALID_CHOICE_ERROR);
-                    break;
+                    default:
+                        System.out.println(INVALID_CHOICE_ERROR);
+                        break;
+                }
             }
+            catch (Exception e) {
+                System.out.println("Invalid input, please enter a valid numerical value.");
+                scanner.nextLine(); // Clear the buffer
+            }
+
+
+
+
+
 
         }
     }
