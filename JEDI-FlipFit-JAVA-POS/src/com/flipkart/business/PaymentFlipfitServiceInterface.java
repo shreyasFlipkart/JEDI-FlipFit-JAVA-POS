9e6218ad2fa6e2356d1b9f7fc7ca7c0d910e9eed
krishnapriyav1 <krishnapriya.v1@flipkart.com>
2024-01-29 17:05:34 +0530

    added comments to CustomerFlipFitClient

--- JEDI-FlipFit-JAVA-POS/src/com/flipkart/business/PaymentFlipfitServiceInterface.java
+++ JEDI-FlipFit-JAVA-POS/src/com/flipkart/business/PaymentFlipfitServiceInterface.java
@@ -7,19 +7,19 @@ package com.flipkart.business;
 public interface PaymentFlipfitServiceInterface {
     /**
      * Method to make payment to confirm the slot booking
-     * @param userName
-     * @param gymCentreId
-     * @param bookingId
+     * @param userName username
+     * @param gymCentreId gym center Id
+     * @param bookingId booking Id
      */
     public void makePayment(String userName, String gymCentreId, String bookingId);
     /**
      * Method to initiate refund for the cancelled slot
-     * @param bookingId
+     * @param bookingId booking Id of the customer
      */
     public void initiateRefund(String bookingId);
     /**
      * Method to verify the payment status
-     * @param paymentId
+     * @param paymentId payment Id
      * @return boolean value of the payment status(True or False)
      */
     public boolean verifyPayment(String paymentId);
