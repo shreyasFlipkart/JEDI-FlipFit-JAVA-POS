42ceaea14ba988bbaca7d26d694f9e21285d5526
ish-chandra-fk <ishita.chandra@flipkart.com>
2024-01-31 00:33:53 +0530

    gymOwnerClient changes

---
+++ JEDI-FlipFit-JAVA-DropWizard/src/main/java/com/flipkart/bean/editProfile.java
@@ -0,0 +1,24 @@
+package com.flipkart.bean;
+
+public class editProfile {
+
+    private String userId;
+    private String valueToUpdate;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getValueToUpdate() {
+        return valueToUpdate;
+    }
+
+    public void setValueToUpdate(String valueToUpdate) {
+        this.valueToUpdate = valueToUpdate;
+    }
+}
